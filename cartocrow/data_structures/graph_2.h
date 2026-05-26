b71571d88e8bf46f4a0721186f928d14e3fb6e62
Meulemans <w.meulemans@tue.nl>
2026-05-26 11:30:20 +0200

    progress on graph_2 structure: vector containers, indexing by default and maps

--- cartocrow/data_structures/graph_2.h
+++ cartocrow/data_structures/graph_2.h
@@ -19,18 +19,38 @@ namespace cartocrow {
 template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> class Graph_2_vertex;
 template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> class Graph_2_edge;
 
-class Graph_vertex_map_base;
+class Graph_map_base;
 template <class G, typename T> class Graph_vertex_map;
+template <class G, typename T> class Graph_edge_map;
+
+template <class G, typename T> class Graph_static_vertex_map;
+template <class G, typename T> class Graph_static_edge_map;
 
 template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> class Graph_2 {
 	friend class Graph_2_vertex<VertexData, EdgeData, CurveTraits>;
 	friend class Graph_2_edge<VertexData, EdgeData, CurveTraits>;
-	friend class Graph_vertex_map_base;
 	template <typename G, typename T> friend class Graph_vertex_map;
+	template <typename G, typename T> friend class Graph_edge_map;
 
   public:
 	using Vertex = Graph_2_vertex<VertexData, EdgeData, CurveTraits>;
+	using Vertex_handle = Vertex*;
+	using Vertex_const_handle = const Vertex*;
+
 	using Edge = Graph_2_edge<VertexData, EdgeData, CurveTraits>;
+	using Edge_handle = Edge*;
+	using Edge_const_handle = const Edge*;
+
+  private:
+	using Vertex_container = std::vector<Vertex_handle>;
+	using Edge_container = std::vector<Edge_handle>;
+
+  public:
+	using Vertex_iterator = Vertex_container::iterator;
+	using Vertex_const_iterator = Vertex_container::const_iterator;
+	using Edge_iterator = Edge_container::iterator;
+	using Edge_const_iterator = Edge_container::const_iterator;
+
 	using Point_2 = CurveTraits::Point_2;
 	using Curve_2 = CurveTraits::Curve_2;
 	using Vertex_data = VertexData;
@@ -39,37 +59,37 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	using Kernel = Curve_traits::Kernel;
 
   private:
-	using Vertex_container = std::list<Vertex>;
-	using Edge_container = std::list<Edge>;
 	Vertex_container m_vertices;
 	Edge_container m_edges;
 	bool m_oriented = false;
 	bool m_sorted = false;
-	bool m_indexed = false;
 
-	std::vector<Graph_vertex_map_base*> m_vertex_maps;
+	std::vector<Graph_map_base*> m_vertex_maps;
+	std::vector<Graph_map_base*> m_edge_maps;
 
-	void addVertexMap(Graph_vertex_map_base* map) {		
+	void add_vertex_map(Graph_map_base* map) {
 		m_vertex_maps.push_back(map);
 	}
 
-	void removeVertexMap(Graph_vertex_map_base* map) {
+	void remove_vertex_map(Graph_map_base* map) {
 		auto pos = std::find(m_vertex_maps.begin(), m_vertex_maps.end(), map);
 		if (pos != m_vertex_maps.end()) {
 			m_vertex_maps.erase(pos);
 		}
 	}
 
-  public:
-	using Vertex_iterator = Vertex_container::iterator;
-	using Vertex_const_iterator = Vertex_container::const_iterator;
-	using Vertex_handle = Vertex_iterator;
-	using Vertex_const_handle = Vertex_const_iterator;
-	using Edge_iterator = Edge_container::iterator;
-	using Edge_const_iterator = Edge_container::const_iterator;
-	using Edge_handle = Edge_iterator;
-	using Edge_const_handle = Edge_const_iterator;
+	void add_edge_map(Graph_map_base* map) {
+		m_edge_maps.push_back(map);
+	}
 
+	void remove_edge_map(Graph_map_base* map) {
+		auto pos = std::find(m_edge_maps.begin(), m_edge_maps.end(), map);
+		if (pos != m_vertex_maps.end()) {
+			m_edge_maps.erase(pos);
+		}
+	}
+
+  public:
 	class Vertex_range {
 		friend class Graph_2<VertexData, EdgeData, CurveTraits>;
 
@@ -106,82 +126,49 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 		}
 	};
 
-	void index_vertices_and_edges() {
-		m_indexed = true;
-		int index = 0;
-		for (auto& v : m_vertices) {
-			v.m_index = index;
-			++index;
-		}
-
-		index = 0;
-		for (auto& e : m_edges) {
-			e.m_index = index;
-			++index;
-		}
-	}
-
-	bool verify_indexed() {
-		int index = 0;
-		for (auto& v : m_vertices) {
-			if (v.m_index != index)
-				return false;
-			++index;
-		}
-		index = 0;
-		for (auto& e : m_edges) {
-			if (e.m_index != index)
-				return false;
-			++index;
-		}
-		return true;
-	}
-
-	bool is_indexed() {
-		assert(!m_indexed || verify_indexed());
-		return m_indexed;
-	}
-
 	Graph_2& operator=(const Graph_2& other) {
 		if (this == &other)
 			return *this;
 
-		m_vertices.clear();
-		m_edges.clear();
+		for (Vertex_handle v : m_vertices) {
+			delete v;
+		}
+		for (Edge_handle e : m_edges) {
+			delete e;
+		}
+		m_vertices.resize(other.number_of_vertices());
+		m_edges.resize(other.number_of_edges());
 
 		m_oriented = other.m_oriented;
 		m_sorted = other.m_sorted;
-		m_indexed = other.m_indexed;
 
-		std::unordered_map<const Vertex*, Vertex_handle> vmap;
+		Graph_static_vertex_map<Graph_2, Vertex_handle> vmap(other, nullptr);
 
-		for (auto vit = other.m_vertices.begin(); vit != other.m_vertices.end(); ++vit) {
-			m_vertices.emplace_back();
-			auto new_vit = std::prev(m_vertices.end());
-			new_vit->point() = vit->point();
-			new_vit->data() = vit->data();
-			new_vit->m_index = vit->m_index;
+		for (auto vit : other.vertices()) {
+			Vertex_handle new_v = new Vertex(vit->m_point, vit->m_data);
+			new_v->m_index = vit->m_index;
+			m_vertices[vit->m_index] = new_v;
 
-			vmap[&*vit] = new_vit;
+			vmap[vit] = new_v;
 		}
 
-		std::unordered_map<const Edge*, Edge_handle> emap;
+		Graph_static_edge_map<Graph_2, Edge_handle> emap(other, nullptr);
 
-		for (auto eit = other.m_edges.begin(); eit != other.m_edges.end(); ++eit) {
-			Vertex_handle new_source = vmap.at(&*eit->m_source);
-			Vertex_handle new_target = vmap.at(&*eit->m_target);
+		for (auto eit : other.edges()) {
+			Vertex_handle new_source = vmap[eit->m_source];
+			Vertex_handle new_target = vmap[eit->m_target];
 
-			auto& e = m_edges.emplace_back(new_source, new_target, eit->m_curve, eit->m_data);
-			e.m_index = eit->m_index;
+			Edge_handle new_e = new Edge(new_source, new_target, eit->m_curve, eit->m_data);
+			new_e->m_index = eit->m_index;
+			m_edges[eit->m_index] = new_e;
 
-			auto new_eit = std::prev(m_edges.end());
-			emap[&*eit] = new_eit;
+			emap[eit] = new_e;
 		}
 
-		for (auto vit = other.m_vertices.begin(); vit != other.m_vertices.end(); ++vit) {
-			Vertex_handle new_v = vmap.at(&*vit);
+		for (auto vit : other.vertices()) {
+			Vertex_handle new_v = vmap[vit];
 			for (auto old_e : vit->m_incident) {
-				new_v->m_incident.push_back(emap.at(&*old_e));
+				new_v->m_incident.push_back(emap[old_e]);
 			}
 		}
 
@@ -197,37 +184,40 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	Graph_2 transform(CGAL::Aff_transformation_2<Kernel> trans) const {
 		Graph_2 transformed;
 
+		transformed.m_vertices.resize(number_of_vertices());
+		transformed.m_edges.resize(number_of_edges());
+
 		transformed.m_oriented = m_oriented;
 		transformed.m_sorted = m_sorted;
 
-		std::unordered_map<const Vertex*, Vertex_handle> vmap;
+		Graph_static_vertex_map<Graph_2, Vertex_handle> vmap(this, nullptr);
 
-		for (auto vit = m_vertices.begin(); vit != m_vertices.end(); ++vit) {
-			transformed.m_vertices.emplace_back();
-			auto new_vit = std::prev(transformed.m_vertices.end());
-			new_vit->point() = vit->point().transform(trans);
-			new_vit->data() = vit->data();
+		for (auto vit : vertices()) {
+			Vertex_handle new_v = new Vertex(vit->m_point.transform(trans), vit->m_data);
+			new_v->m_index = vit->m_index;
+			transformed.m_vertices[vit->m_index] = new_v;
 
-			vmap[&*vit] = new_vit;
+			vmap[vit] = new_v;
 		}
 
-		std::unordered_map<const Edge*, Edge_handle> emap;
+		Graph_static_edge_map<Graph_2, Edge_handle> emap(this, nullptr);
 
-		for (auto eit = m_edges.begin(); eit != m_edges.end(); ++eit) {
-			Vertex_handle new_source = vmap.at(&*eit->m_source);
-			Vertex_handle new_target = vmap.at(&*eit->m_target);
+		for (auto eit : edges()) {
+			Vertex_handle new_source = vmap[eit->m_source];
+			Vertex_handle new_target = vmap[eit->m_target];
 
-			transformed.m_edges.emplace_back(
-			    new_source, new_target, Curve_traits::transform(eit->m_curve, trans), eit->m_data);
+			Edge_handle new_e = new Edge(new_source, new_target,
+			                             Curve_traits::transform(eit->m_curve, trans), eit->m_data);
+			new_e->m_index = eit->m_index;
+			transformed.m_edges[eit->m_index] = new_e;
 
-			auto new_eit = std::prev(transformed.m_edges.end());
-			emap[&*eit] = new_eit;
+			emap[eit] = new_e;
 		}
 
-		for (auto vit = m_vertices.begin(); vit != m_vertices.end(); ++vit) {
-			Vertex_handle new_v = vmap.at(&*vit);
+		for (auto vit : vertices()) {
+			Vertex_handle new_v = vmap[vit];
 			for (auto old_e : vit->m_incident) {
-				new_v->m_incident.push_back(emap.at(&*old_e));
+				new_v->m_incident.push_back(emap[old_e]);
 			}
 		}
 
@@ -273,65 +263,92 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	size_t number_of_edges() const {
 		return m_edges.size();
 	}
+
 	void clear() {
-		m_vertices.clear();
-		m_edges.clear();
-		m_oriented = false;
-		m_sorted = false;
+		int last = m_vertices.size() - 1;
+		while (last >= 0) {
+			remove_vertex(m_vertices[last]);
+			last--;
+		}
 	}
+
 	Vertex_handle add_vertex(Point_2 p) {
-		Vertex& v = m_vertices.emplace_back();
-		v.m_point = std::move(p);
-		if (m_indexed) {
-			v.m_index = m_vertices.size() - 1;
+		Vertex_handle v = new Vertex(p);
+		const int index = v->m_index = m_vertices.size();
+		m_vertices.push_back(v);
+		for (Graph_map_base* m : m_vertex_maps) {
+			m->index_updated(-1, index);
 		}
-		for (Graph_vertex_map_base* m : m_vertex_maps) {
-			m->index_updated(-1, v.m_index);
-		}
-		return (--m_vertices.end());
+		return v;
 	}
 	void remove_vertex(Vertex_handle v) {
-		int index = v->m_index;
-		v->m_index = -1;
-		for (Graph_vertex_map_base* m : m_vertex_maps) {
+		const int index = v->m_index;
+		for (Graph_map_base* m : m_vertex_maps) {
 			m->index_updated(index, -1);
 		}
-
-		m_indexed = false;
 		for (Edge_handle e : v->m_incident) {
 			remove_edge(e);
 		}
-		m_vertices.erase(v);
+		const int last = m_vertices.size() - 1;
+		if (index != last) {
+			m_vertices[index] = m_vertices[last];
+			m_vertices[index]->m_index = index;
+
+			for (Graph_map_base* m : m_vertex_maps) {
+				m->index_updated(last, index);
+			}
+		}
+		m_vertices.pop_back();
+		delete v;
 	}
 	Edge_handle add_edge(Vertex_handle source, Vertex_handle target, const Curve_2& curve) {
-		Edge& e = m_edges.emplace_back(std::move(source), std::move(target), std::move(curve));
-		Edge_handle eh = --m_edges.end();
-		Vertex_handle es = e.m_source;
-		Vertex_handle et = e.m_target;
+		Edge_handle e = new Edge(source, target, curve);
+		const int index = e->m_index = m_edges.size();
+		m_edges.push_back(e);
+
 		m_sorted = false;
 
-		std::vector<Edge_handle>& esInc = es->m_incident;
-		std::vector<Edge_handle>& etInc = et->m_incident;
+		std::vector<Edge_handle>& source_inc = source->m_incident;
+		std::vector<Edge_handle>& target_inc = target->m_incident;
+		
 		// Make sure orientation is preserved
-		esInc.push_back(eh);
-		if (etInc.empty()) {
-			etInc.push_back(eh);
+		source_inc.push_back(e);
+		if (target_inc.empty()) {
+			target_inc.push_back(e);
 		} else {
-			etInc.push_back(etInc.front());
-			etInc[0] = eh;
+			target_inc.push_back(target_inc.front());
+			target_inc[0] = e;
 		}
-		if (m_indexed) {
-			eh->m_index = m_edges.size() - 1;
+
+		for (Graph_map_base* m : m_edge_maps) {
+			m->index_updated(-1, index);
 		}
-		return eh;
+
+		return e;
 	}
-	void remove_edge(Edge_handle edge) {
-		m_indexed = false;
-		auto& sInc = edge->m_source->m_incident;
-		auto& tInc = edge->m_target->m_incident;
-		utils::vectorRemove(edge, sInc);
-		utils::vectorRemove(edge, tInc);
-		m_edges.erase(edge);
+	void remove_edge(Edge_handle e) {
+
+		const int index = e->m_index;
+		for (Graph_map_base* m : m_edge_maps) {
+			m->index_updated(index, -1);
+		}
+
+		auto& sInc = e->m_source->m_incident;
+		auto& tInc = e->m_target->m_incident;
+		utils::vectorRemove(e, sInc);
+		utils::vectorRemove(e, tInc);
+
+		const int last = m_edges.size() - 1;
+		if (index != last) {
+			m_edges[index] = m_edges[last];
+			m_edges[index]->m_index = index;
+
+			for (Graph_map_base* m : m_edge_maps) {
+				m->index_updated(last, index);
+			}
+		}
+		m_edges.pop_back();
+		delete e;
 	}
 	/// Split a vertex into two. Or equivalently, replace two curves by three new ones.
 	/// Connect the two new vertices with three new curves.
@@ -512,7 +529,7 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 		m_sorted = true;
 	}
 	bool verify_sorted() {
-		for (const Vertex_const_handle& v : m_vertices) {
+		for (const Vertex_const_handle v : m_vertices) {
 			if (v->degree() > 2) {
 				CGAL::Direction_2<Kernel> dir_prev =
 				    CGAL::Direction_2<Kernel>(v->neighbor(0)->getPoint() - v->getPoint());
@@ -538,7 +555,7 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 			return {0, 0, 0, 0};
 		}
 		std::vector<Point<Inexact>> points;
-		for (const auto& v : m_vertices) {
+		for (Vertex_handle v : m_vertices) {
 			points.push_back(v.point());
 		}
 		auto box = CGAL::bbox_2(points.begin(), points.end());
@@ -552,6 +569,7 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> class Graph_2_vertex {
 	friend class Graph_2<VertexData, EdgeData, CurveTraits>;
 	friend class Graph_2_edge<VertexData, EdgeData, CurveTraits>;
+
   public:
 	using Vertex = Graph_2_vertex<VertexData, EdgeData, CurveTraits>;
 	using Edge = Graph_2_edge<VertexData, EdgeData, CurveTraits>;
@@ -572,8 +590,15 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	Vertex_data m_data;
 	size_t m_index;
 
+	Graph_2_vertex(const Point_2 point) : m_point(std::move(point)) {}
+	Graph_2_vertex(const Point_2 point, const Vertex_data data)
+	    : m_point(std::move(point)), m_data(std::move(data)) {}
+
   public:
-	size_t graph_index() const {
+	size_t graph_index() {
+		return m_index;
+	}
+	const size_t graph_index() const {
 		return m_index;
 	}
 	const Point_2& point() const {
@@ -658,20 +683,27 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	EdgeData m_data;
 	size_t m_index;
 
-  public:
-	Graph_2_edge(Vertex_handle source, Vertex_handle target, Curve_2 curve)
+	Graph_2_edge(Vertex_handle source, Vertex_handle target, const Curve_2 curve)
 	    : m_source(std::move(source)), m_target(std::move(target)), m_curve(std::move(curve)) {
 		assert(m_curve.source() == m_source->m_point);
 		assert(m_curve.target() == m_target->m_point);
-	};
+	}
 
-	Graph_2_edge(Vertex_handle source, Vertex_handle target, Curve_2 curve, Edge_data d)
+	Graph_2_edge(Vertex_handle source, Vertex_handle target, const Curve_2 curve, const Edge_data d)
 	    : m_source(std::move(source)), m_target(std::move(target)), m_curve(std::move(curve)),
-	      m_data(std::move(d)) {};
+	      m_data(std::move(d)) {
+		assert(m_curve.source() == m_source->m_point);
+		assert(m_curve.target() == m_target->m_point);
+	}
 
-	size_t graph_index() const {
+  public:
+	size_t graph_index() {
 		return m_index;
 	}
+	const size_t graph_index() const {
+		return m_index;
+	}
+
 	Vertex_handle source() {
 		return m_source;
 	}
@@ -718,23 +750,20 @@ template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> clas
 	}
 };
 
-class Graph_vertex_map_base {
+class Graph_map_base {
 
-	template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits> 
+	template <class VertexData, class EdgeData, GraphCurveTraits_2 CurveTraits>
 	friend class Graph_2;
 
   protected:
 	virtual void index_updated(const int old_index, const int new_index) {}
 };
 
-template <class G, typename T> class Graph_vertex_map : public Graph_vertex_map_base {
-	
+template <class G, class E, typename T> class Graph_map : public Graph_map_base {
+
 	friend G;
 
-  public:
-	using Vertex_handle = G::Vertex_handle;
-
-  private:
+  protected:
 	G& m_graph;
 	std::vector<T> m_vec;
 	const T m_init;
@@ -745,12 +774,11 @@ template <class G, typename T> class Graph_vertex_map : public Graph_vertex_map_
 		}
 	}
 
-protected:
 	void index_updated(const int old_index, const int new_index) override {
 		if (old_index < 0) {
 			// new vertex
 			if (m_vec.size() <= new_index) {
-				m_vec.resize(new_index+1, m_init);
+				m_vec.resize(new_index + 1, m_init);
 			}
 
 			m_vec[new_index] = m_init;
@@ -767,31 +795,39 @@ protected:
 	}
 
   public:
-	Graph_vertex_map(G& graph, const T init) : m_init(init), m_graph(graph) {
-		m_graph.addVertexMap(this);
-		m_vec.resize(m_graph.number_of_vertices(), m_init);
+	Graph_map(G& graph, const T init, int cnt) : m_init(init), m_graph(graph) {
+		m_vec.resize(cnt, m_init);
+	}
+
+	T& operator[](const E elt) {
+		return m_vec[elt->graph_index()];
+	}
+};
+
+template <class G, typename T>
+class Graph_vertex_map : public Graph_map<G, typename G::Vertex_handle, T> {
+
+  public:
+	Graph_vertex_map(G& graph, const T init)
+	    : Graph_map<G, G::Vertex_handle, T>(graph, init, graph.number_of_vertices()) {
+		this->m_graph.add_vertex_map(this);
 	}
 
 	~Graph_vertex_map() {
-		m_graph.removeVertexMap(this);
-	}
-
-	T& operator[](const Vertex_handle vtx) {
-		return m_vec[vtx->graph_index()];
+		this->m_graph.remove_vertex_map(this);
 	}
 };
 
 template <class G, typename T> class Graph_static_vertex_map {
 
   public:
-	using Graph = G;
 	using Vertex_handle = G::Vertex_handle;
 
   private:
 	std::vector<T> m_vec;
 
   public:
-	Graph_static_vertex_map(Graph& graph, const T init) {
+	Graph_static_vertex_map(G& graph, const T init) {
 		m_vec.resize(graph.number_of_vertices(), init);
 	}
 
@@ -802,13 +838,51 @@ template <class G, typename T> class Graph_static_vertex_map {
 
 template <class G> class Graph_vertex_index_map {
   public:
-	using Graph = G;
 	using Vertex_handle = G::Vertex_handle;
-
-	Graph_vertex_index_map(Graph& graph) {}
 
 	size_t operator[](const Vertex_handle vtx) {
 		return vtx->graph_index();
+	}
+};
+
+template <class G, typename T>
+class Graph_edge_map : public Graph_map<G, typename G::Edge_handle, T> {
+
+  public:
+	Graph_edge_map(G& graph, const T init)
+	    : Graph_map<G, typename G::Edge_handle, T>(graph, init, graph.number_of_edges()) {
+		this->m_graph.add_edge_map(this);
+	}
+
+	~Graph_edge_map() {
+		this->m_graph.remove_edge_map(this);
+	}
+};
+
+template <class G, typename T> class Graph_static_edge_map {
+
+  public:
+	using Edge_handle = G::Edge_handle;
+
+  private:
+	std::vector<T> m_vec;
+
+  public:
+	Graph_static_edge_map(G& graph, const T init) {
+		m_vec.resize(graph.number_of_edges(), init);
+	}
+
+	T& operator[](const Edge_handle edge) {
+		return m_vec[edge->graph_index()];
+	}
+};
+
+template <class G> class Graph_edge_index_map {
+  public:
+	using Edge_handle = G::Edge_handle;
+
+	size_t operator[](const Edge_handle edge) {
+		return edge->graph_index();
 	}
 };
 
--- test/data_structures/graph_2.cpp
+++ test/data_structures/graph_2.cpp
@@ -14,8 +14,6 @@ TEST_CASE("Graph static vertex map") {
 	G g;
 	V u = g.add_vertex(P(0, 0));
 	V v = g.add_vertex(P(0, 0));
-	g.index_vertices_and_edges();
-
 
 	Graph_static_vertex_map<G,int> map(g, 1);
 
@@ -42,11 +40,8 @@ TEST_CASE("Graph vertex map") {
 	G g;
 	V u = g.add_vertex(P(0, 0));
 	V v = g.add_vertex(P(0, 0));
-	g.index_vertices_and_edges();
 
 	Graph_vertex_map<G, int> map(g, 1);
-
-
 
 	CHECK(map[u] == 1);
 	CHECK(map[v] == 1);
@@ -60,4 +55,70 @@ TEST_CASE("Graph vertex map") {
 	g.remove_vertex(u);
 
 	CHECK(map[v] == 2);
+
+	map[v] = 5;
+
+	CHECK(map[v] == 5);
+}
+
+TEST_CASE("Graph static edge map") {
+
+	using G = Straight_graph_2<std::monostate, std::monostate, Exact>;
+	using V = G::Vertex_handle;
+	using E = G::Edge_handle;
+	using P = Point<Exact>;
+
+	G g;
+	V u = g.add_vertex(P(0, 0));
+	V v = g.add_vertex(P(0, 0));
+	V w = g.add_vertex(P(0, 0));
+
+	E e = g.add_edge(u, v, Segment<Exact>(P(0, 0), P(0, 0)));
+	E f = g.add_edge(v, w, Segment<Exact>(P(0, 0), P(0, 0)));
+
+	Graph_static_edge_map<G, int> map(g, 1);
+
+	CHECK(map[e] == 1);
+	CHECK(map[f] == 1);
+
+	map[e] = 3;
+	map[f] = 2;
+
+	CHECK(map[e] == 3);
+	CHECK(map[f] == 2);
+}
+
+TEST_CASE("Graph edge map") {
+
+	using G = Straight_graph_2<std::monostate, std::monostate, Exact>;
+	using V = G::Vertex_handle;
+	using E = G::Edge_handle;
+	using P = Point<Exact>;
+
+	G g;
+	V u = g.add_vertex(P(0, 0));
+	V v = g.add_vertex(P(0, 0));
+	V w = g.add_vertex(P(0, 0));
+
+	E e = g.add_edge(u, v, Segment<Exact>(P(0, 0), P(0, 0)));
+	E f = g.add_edge(v, w, Segment<Exact>(P(0, 0), P(0, 0)));
+
+	Graph_edge_map<G, int> map(g, 1);
+
+	CHECK(map[e] == 1);
+	CHECK(map[f] == 1);
+
+	map[e] = 3;
+	map[f] = 2;
+
+	CHECK(map[e] == 3);
+	CHECK(map[f] == 2);
+
+	g.remove_edge(e);
+
+	CHECK(map[f] == 2);
+
+	map[f] = 5;
+
+	CHECK(map[f] == 5);
 }
